61bbda90c36def81103d0d4bc53104c1e789997b
Ismail-Taha <taha.ismaile2002@gmail.com>
2024-05-30 22:07:29 +0100

    done if

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -8,6 +8,7 @@ def pascal_triangle(n):
     """Create a function def pascal_triangle(n): that returns a list of lists
     of integers representing the Pascal’s triangle of n
     """
+
     triangle = []
     if n > 0:
         for i in range(1, n + 1): # Initialize the first element of the row to 1 (binomial coefficient)
